4fd8e65ec474e660ad19f1be3c0e831a3ff9ea07
Farzadkalateh <senior5027@gmail.com>
2025-12-11 09:54:01 +0330

    login and singup added

--- accunt/urls.py
+++ accunt/urls.py
@@ -3,11 +3,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'accunt' # این خط برای namespace ضروریه
+app_name = 'accunt' 
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"), # به views.login_view تغییر یافت
+    path("login/", views.login_view, name="login"), 
     path("logout/", views.logout_user, name="logout"),
     path("signup/", views.sign_up, name="signup"),
-    path("profile/", views.profile_view, name="profile"), # این خط جدید اضافه شد
+    path("profile/", views.profile_view, name="profile"),
 ]
